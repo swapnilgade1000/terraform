824e8c08b4fa9717d6a92fa15bae9a488eef3812
swapnilgade1000 <swapnilgade100@gmail.com>
2024-08-31 17:00:07 +0530

    w

--- bucket creation.tf
+++ bucket creation.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 #creating bucket
 resource "aws_s3_bucket"  "this_sb" {
-        bucket = "swapnil_chutiya"
+        bucket = "swapniladdu"
         object_lock_enabled = true
         force_destroy = true
         tags = {
