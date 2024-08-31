768a57b130eb0e2555cb870da9064eb1c65f2a25
swapnilgade1000 <swapnilgade100@gmail.com>
2024-08-31 16:48:35 +0530

    n

--- bucket creation.tf
+++ bucket creation.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.region
+}
+
+
 #creating bucket
 resource "aws_s3_bucket"  "this_sb" {
         bucket = "terraformbasedbucket"
